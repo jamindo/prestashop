f2f5535a075bcc57c09d00a1423ccc0594265469
jamindo <benjamin.german@sepefrei.fr>
2014-01-06 18:30:33 +0100

    dqs

--- cache/smarty/compile/dd/12/b6/dd12b644e76ca94c64fe2e5b92d87aa6f481282b.file.blockuserinfo.tpl.php
+++ cache/smarty/compile/dd/12/b6/dd12b644e76ca94c64fe2e5b92d87aa6f481282b.file.blockuserinfo.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.14, created on 2013-10-31 17:24:16
+<?php /* Smarty version Smarty-3.1.14, created on 2014-01-06 18:29:49
          compiled from "C:\xampp\htdocs\prestashop\modules\blockuserinfo\blockuserinfo.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:1790052728430a8f054-85627252%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'dd12b644e76ca94c64fe2e5b92d87aa6f481282b' => 
     array (
       0 => 'C:\\xampp\\htdocs\\prestashop\\modules\\blockuserinfo\\blockuserinfo.tpl',
-      1 => 1381145310,
+      1 => 1389029291,
       2 => 'file',
     ),
   ),
@@ -15,6 +15,8 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'function' => 
   array (
   ),
+  'version' => 'Smarty-3.1.14',
+  'unifunc' => 'content_52728430b781b6_40279541',
   'variables' => 
   array (
     'PS_CATALOG_MODE' => 0,
@@ -28,8 +30,6 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'cookie' => 0,
   ),
   'has_nocache_code' => false,
-  'version' => 'Smarty-3.1.14',
-  'unifunc' => 'content_52728430b781b6_40279541',
 ),false); /*/%%SmartyHeaderCode%%*/?>
 <?php if ($_valid && !is_callable('content_52728430b781b6_40279541')) {function content_52728430b781b6_40279541($_smarty_tpl) {?>
 
